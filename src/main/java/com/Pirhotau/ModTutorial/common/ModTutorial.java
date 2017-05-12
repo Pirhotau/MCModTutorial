9fd18c6154a49d38d781d4b90355c98e78d3a6ef
Remy BLANC <remyblanc@hotmail.fr>
2017-05-13 01:16:29 +0200

    Some adds in the main class (events)

--- src/main/java/com/Pirhotau/ModTutorial/common/ModTutorial.java
+++ src/main/java/com/Pirhotau/ModTutorial/common/ModTutorial.java
@@ -1,7 +1,11 @@
 package com.Pirhotau.ModTutorial.common;
 
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = ModTutorial.MODID, name = ModTutorial.MOD_NAME, version = ModTutorial.VERSION)
 
@@ -9,7 +13,33 @@ public class ModTutorial {
 	public static final String MODID = "modtutorial";
 	public static final String MOD_NAME = "Tutorial mod";
 	public static final String VERSION = "0.0.1";
-	
+
 	@Instance(MODID)
 	public static ModTutorial instance;
+
+	/*
+	 * Read config file
+	 * Register blocks and items
+	 */
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+
+	}
+
+	/*
+	 * Register events
+	 * Register recipes
+	 */
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+
+	}
+	
+	/*
+	 * Interaction with other mods
+	 */
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent event) {
+
+	}
 }
