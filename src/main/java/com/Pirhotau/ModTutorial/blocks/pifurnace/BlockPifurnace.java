02eaa214454e018fb6c577b3c880bd639c570d76
Rémy Blanc <remyblanc@hotmail.fr>
2017-07-03 18:45:56 +0200

    A really simple tile entity: the Pifurnace

--- src/main/java/com/Pirhotau/ModTutorial/blocks/pifurnace/BlockPifurnace.java
+++ src/main/java/com/Pirhotau/ModTutorial/blocks/pifurnace/BlockPifurnace.java
@@ -3,41 +3,17 @@ package com.Pirhotau.ModTutorial.blocks.pifurnace;
 import com.Pirhotau.ModTutorial.blocks.BlockTileEntity;
 
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.EnumHand;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.World;
 
 public class BlockPifurnace extends BlockTileEntity {
-	
+
 	public BlockPifurnace() {
 		super("pifurnace");
 	}
 
 	@Override
-	public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn,
-			EnumHand hand, ItemStack heldItem, EnumFacing side, float hitX, float hitY, float hitZ) {
-		
-		if(!worldIn.isRemote) {
-			TileEntityPifurnace tile = (TileEntityPifurnace) getTileEntity(worldIn, pos);
-			
-			if(side == EnumFacing.WEST) tile.decrement();
-			else if(side == EnumFacing.EAST) tile.increment();
-			
-			playerIn.addChatComponentMessage(new TextComponentString("Count: " + tile.getCount()));
-		}
-		
-		return true;
-	}
-	
-	
-	
-	@Override
-	public Class getTileEntityClass() {
+	public Class<TileEntityPifurnace> getTileEntityClass() {
 		return TileEntityPifurnace.class;
 	}
 
--- src/main/java/com/Pirhotau/ModTutorial/blocks/pifurnace/TileEntityPifurnace.java
+++ src/main/java/com/Pirhotau/ModTutorial/blocks/pifurnace/TileEntityPifurnace.java
@@ -4,33 +4,15 @@ import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 
 public class TileEntityPifurnace extends TileEntity {
-	
-	private int count;
-	
+
 	@Override
 	public NBTTagCompound writeToNBT(NBTTagCompound compound) {
-		compound.setInteger("count", count);
 		return super.writeToNBT(compound);
 	}
-	
+
 	@Override
 	public void readFromNBT(NBTTagCompound compound) {
-		count = compound.getInteger("count");
 		super.readFromNBT(compound);
-	}
-	
-	public void increment() {
-		count ++;
-		markDirty();
-	}
-	
-	public void decrement() {
-		count --;
-		markDirty();
-	}
-	
-	public int getCount() {
-		return count;
 	}
 
 }
